193b25e3e0b6248fdd1c3865a7d2e41d2511ec5e
Victus <abdulbositkhalikov96@gmail.com>
2024-03-17 20:11:11 +0500

    Some changes

--- main/views.py
+++ main/views.py
@@ -20,7 +20,7 @@ def foods(request):
             'images': [image.image for image in food.images.all()],
             'name': food.name,
             'description': food.description,
-            'rating': round(mean([float(rating) for rating in food.comments.values_list('rating', flat=True)]), 1),
+            'rating': round(mean([float(rating) for rating in food.comments.values_list('rating', flat=True)]), 1) if food.comments else 0,
             'comments': [{
                 'id': comment.id,
                 'author': {
@@ -57,7 +57,7 @@ def foods_by_category(request, category):
             'images': [image.image for image in food.images.all()],
             'name': food.name,
             'description': food.description,
-            'rating': round(mean([float(rating) for rating in food.comments.values_list('rating', flat=True)]), 1),
+            'rating': round(mean([float(rating) for rating in food.comments.values_list('rating', flat=True)]), 1) if food.comments else 0,
             'comments': [{
                 'id': comment.id,
                 'author': {
@@ -94,7 +94,7 @@ def hotels(request):
             'name': hotel.name,
             'images': [image.image for image in hotel.images.all()],
             'description': hotel.description,
-            'rating': round(mean([float(rating) for rating in hotel.comments.values_list('rating', flat=True)]), 1),
+            'rating': round(mean([float(rating) for rating in hotel.comments.values_list('rating', flat=True)]), 1) if hotel.comments else 0,
             'comments': [{
                 'id': comment.id,
                 'author': {
@@ -130,7 +130,7 @@ def hotels_by_category(request, category):
             'name': hotel.name,
             'images': [image.image for image in hotel.images.all()],
             'description': hotel.description,
-            'rating': round(mean([float(rating) for rating in hotel.comments.values_list('rating', flat=True)]), 1),
+            'rating': round(mean([float(rating) for rating in hotel.comments.values_list('rating', flat=True)]), 1) if hotel.comments else 0,
             'comments': [{
                 'id': comment.id,
                 'author': {
@@ -183,7 +183,7 @@ def destinations(request):
             'name': destination.name,
             'description': destination.description,
             'history': destination.history,
-            'ratings': round(mean([float(rating) for rating in destination.comments.values_list('rating', flat=True)]), 1),
+            'ratings': round(mean([float(rating) for rating in destination.comments.values_list('rating', flat=True)]), 1) if destination.comments else 0,
             'comments': [{
                 'id': comment.id,
                 'author': {
@@ -215,7 +215,7 @@ def destinations_by_category(request, category):
             'name': destination.name,
             'description': destination.description,
             'history': destination.history,
-            'rating': round(mean([float(rating) for rating in destination.comments.values_list('rating', flat=True)]), 1),
+            'rating': round(mean([float(rating) for rating in destination.comments.values_list('rating', flat=True)]), 1) if destination.comments else 0,
             'comments': [{
                 'id': comment.id,
                 'author': {
@@ -254,7 +254,7 @@ def destinations_by_state(request, state):
             'name': destination.name,
             'description': destination.description,
             'history': destination.history,
-            'rating': round(mean([float(rating) for rating in destination.comments.values_list('rating', flat=True)]), 1),
+            'rating': round(mean([float(rating) for rating in destination.comments.values_list('rating', flat=True)]), 1) if destination.comments else 0,
             'comments': [{
                 'id': comment.id,
                 'author': {
